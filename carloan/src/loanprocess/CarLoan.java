c0dc57563f48d2dda4ea7509704a70a95661757b
sriji <sriji@LAPTOP-KMR67EL8>
2024-01-28 01:11:10 +0530

    changed

--- carloan/src/loanprocess/CarLoan.java
+++ carloan/src/loanprocess/CarLoan.java
@@ -4,7 +4,7 @@ public class CarLoan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  System.out.print("car loan process needed");
+  System.out.print("car loan pushed");
 	}
 
 }
