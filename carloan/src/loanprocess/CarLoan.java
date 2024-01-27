876d115872f37996839abf618a7de208099fca56
sriji <sriji@LAPTOP-KMR67EL8>
2024-01-28 00:46:55 +0530

    carloan testcase

---
+++ carloan/src/loanprocess/CarLoan.java
@@ -0,0 +1,10 @@
+package loanprocess;
+
+public class CarLoan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.print("car loan process needed");
+	}
+
+}
